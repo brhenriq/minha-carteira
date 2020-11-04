688821ade66df9d50c29a8405cd7530ce00f5189
Bruno Santos <brunohqsantos@hotmail.com>
2020-11-03 22:38:16 -0300

    refactor: update message api running

--- backend/src/infra/http/server.js
+++ backend/src/infra/http/server.js
@@ -6,4 +6,6 @@ const server = express();
 server.use(express.json());
 server.use(routes);
 
-server.listen(3333);
+server.listen(3333, function(){
+  console.log('Server is running on 3333 port!')
+});
