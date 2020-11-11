39f21faa0c1f69e879095952e264dc4e20bae457
Bruno Santos <brunohqsantos@hotmail.com>
2020-11-11 20:58:45 -0300

    feat: code coverage report

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   clearMocks: true,
   coverageProvider: 'v8',
   testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['<rootDir>/src/modules/*/service/*.js'],
 };
